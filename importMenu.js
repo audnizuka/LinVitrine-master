ea0d534cbcf8dae6ebb3c9e9803db267ff9c63ba
Audrey <103561771+audnizuka@users.noreply.github.com>
2022-12-21 11:45:36 +0100

    ajax click

--- importMenu.js
+++ importMenu.js
@@ -1,1 +1,64 @@
-$("#menu").load("ajax/menu.html");
+//var sidenav = document.getElementById("mySidenav");
+
+//function openNav() {
+  //  $("#mySidenav").classList.add("active");
+//}
+
+
+//function closeNav() {
+ //   sidenav.classList.remove("active");
+//function closeNav() {
+ //   sidenav.classList.remove("active");
+//}
+
+
+//$("#menu").load("ajax/menu.html");
+
+
+
+$.ajax({
+    method: "GET",
+    url: "ajax/menu.html",
+    dataType: "html"
+})
+    .done(function (data) {
+        $("#menu").html(data);
+        $("#openBtn").on("click", function() {
+            $("mySidenav").classList.add("active");
+        });
+        $("#closeBtn").on("click", function() {
+            console.log( $( this ).text("cvfcd") );
+        });
+    });
+
+
+
+
+
+
+        //       var openBtn = document.getElementById("openBtn");
+        //      var closeBtn = document.getElementById("closeBtn");
+
+        //        $("#openBtn").on("click",openNav);
+        //        $("#closeBtn").on("click", closeNav);
+
+
+        //  closeBtn.onclick = closeNav;
+
+
+        
+//$.ajax({
+  //  method: "GET",
+    //url: "ajax/menu.html",
+//    dataType: "html"
+//})
+//    .done(function (data) {
+//        $("#menu").html(data);
+//        $("openBtn").click(function (event) {
+//            openNav(event);
+//        });
+//        $("closeBtn").click(function (event) {
+//            closeNav(event);
+//        });
+//
+//    });
